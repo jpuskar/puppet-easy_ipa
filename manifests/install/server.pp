68df15573bea00c34266fced585b86074a985d98
gdnico <31092984+gdnico@users.noreply.github.com>
2017-08-28 10:28:11 +0200

    correct bug

--- manifests/install/server.pp
+++ manifests/install/server.pp
@@ -68,23 +68,23 @@ class easy_ipa::install::server {
     contain 'easy_ipa::install::server::master'
     #install role AD trust controller
     if easy_ipa::server_role_adtrustcontroller {
-      -> contain 'easy_ipa::install::server::role::adtrustcontroller' 
+      contain 'easy_ipa::install::server::role::adtrustcontroller' 
     } 
     if easy_ipa::server_role_adtrustagent {
-      -> contain 'easy_ipa::install::server::role::adtrustagent'
+      contain 'easy_ipa::install::server::role::adtrustagent'
     }
   } elsif $easy_ipa::ipa_role == 'replica' {
     contain 'easy_ipa::install::server::replica'
     #install role AD trust controller
     if easy_ipa::server_role_adtrustcontroller {
-      -> contain 'easy_ipa::install::server::role::adtrustcontroller'  
+      contain 'easy_ipa::install::server::role::adtrustcontroller'  
     }
     if easy_ipa::server_role_adtrustagent {
-      -> contain 'easy_ipa::install::server::role::adtrustagent'
+      contain 'easy_ipa::install::server::role::adtrustagent'
     }
     #install role ca
     if easy_ipa::server_role_ca {
-      -> contain 'easy_ipa::install::server::role::ca'
+      contain 'easy_ipa::install::server::role::ca'
     }
   }
 
