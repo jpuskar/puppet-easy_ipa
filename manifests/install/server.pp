317c608a13f94475fc8331b8d9269de7d4c1235c
gdnico <31092984+gdnico@users.noreply.github.com>
2017-09-05 15:01:18 +0200

    correct bug

--- manifests/install/server.pp
+++ manifests/install/server.pp
@@ -114,6 +114,7 @@ class easy_ipa::install::server {
   }
 
   easy_ipa::helpers::flushcache { "server_${easy_ipa::ipa_server_fqdn}": }
-  class {'easy_ipa::config::admin_user': }
-
+  if $easy_ipa::install_kstart {
+    class {'easy_ipa::config::admin_user': }
+  }
 }
