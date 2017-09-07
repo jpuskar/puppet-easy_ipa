f91c3ce50d5665ac2fa8c85e25910d4aa03022e1
gdnico <31092984+gdnico@users.noreply.github.com>
2017-09-07 17:05:09 +0200

    test

--- manifests/install/server/role/adtrustcontroller.pp
+++ manifests/install/server/role/adtrustcontroller.pp
@@ -23,7 +23,7 @@ class easy_ipa::install::server::role::adtrustcontroller {
         unless    => "/usr/bin/kinit -t /etc/krb5.keytab;/usr/bin/ipa trustconfig-show | grep -wqF ${easy_ipa::ipa_server_fqdn}",
       }
       -> exec { "server_install_${easy_ipa::ipa_server_fqdn}_connection_to_AD":
-        command   => "/usr/bin/echo -e '#!/usr/bin/expect -f \n spawn ${command} \n expect \u0022Active Directory domain administrator\u0027s password: \u0022 \n' > /tmp/reg_ad.sh;/usr/bin/chmod 755 /tmp/reg_ad.sh;",
+        command   => "/usr/bin/echo '#!/usr/bin/expect -f \n spawn ${command} \n expect \u0022Active Directory domain administrator\u0027s password: \u0022 \n' > /tmp/reg_ad.sh;/usr/bin/chmod 755 /tmp/reg_ad.sh;",
         timeout   => 0,
         require   => Package['expect'],
         logoutput => 'on_failure',
